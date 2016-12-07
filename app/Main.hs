ffb34243afea9298fb64423d1b6d92531196692a
Jérôme Mahuet <jerome.mahuet@gmail.com>
2016-12-07 12:38:50 +0100

    day 5

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,4 @@ module Main where
 import Day5
 
 main :: IO ()
-main = day5
+main = day5'
--- src/Day5.hs
+++ src/Day5.hs
@@ -2,11 +2,16 @@
 
 module Day5
     ( day5
+    , day5'
     ) where
 
+import           Control.Lens
+import           Control.Monad
 import           Crypto.Hash.MD5
-import qualified Data.ByteString as B
+import qualified Data.ByteString        as B
 import           Data.ByteString.Base16
+import           Data.Char
+import           Data.Maybe
 import           Shared.ByteString
 
 {- Day 5: How About a Nice Game of Chess? -}
@@ -33,5 +38,26 @@ day5 = print $ concatMap (bsToStr . (5 !!•)) . take 8 . filter isGoodHash . ha
 
 {- Part Two -}
 
+initAnswer :: [Maybe Char]
+initAnswer = replicate 8 Nothing
+
+answerFoundDigit :: [Maybe Char] -> Char -> Char -> [Maybe Char]
+answerFoundDigit ans index d
+  | not (isDigit index) = ans
+  | read [index] > 7 = ans
+  | otherwise =
+    case ans !! read [index] of
+      Nothing -> ans & element (read [index]) .~ Just d
+      _ -> ans
+
 day5' :: IO ()
-day5' = undefined
+day5' =
+  foldM_ computeF initAnswer $ filter isGoodHash . hashes $ puzzleInput
+  where
+    computeF :: [Maybe Char] -> B.ByteString -> IO [Maybe Char]
+    computeF answers hash = do
+      let index = head $ bsToStr (5 !!• hash)
+      let value = head $ bsToStr (6 !!• hash)
+      let newAnswers = answerFoundDigit answers index value
+      print $ catMaybes newAnswers
+      return newAnswers
